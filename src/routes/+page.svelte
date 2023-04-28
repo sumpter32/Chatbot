6f2d5b86f41e63810e92201e0f67efcca35d3df7
Steven S <sumpter32@gmail.com>
2023-04-29 07:59:19 +0800

    22Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import ChatMessage from '$lib/components/ChatMessage.svelte'
   import type { ChatCompletionRequestMessage } from 'openai'
   import { SSE } from 'sse.js'
