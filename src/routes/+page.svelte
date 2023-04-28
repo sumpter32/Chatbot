4db7767699f763ef858790184029182be10147f4
Steven S <sumpter32@gmail.com>
2023-04-29 06:41:23 +0800

    2Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,12 @@
 	let query: string = ''
 	let answer: string = ''
 	let loading: boolean = false
-	let chatMessages: ChatCompletionRequestMessage[] = []
+	let chatMessages: ChatCompletionRequestMessage[] = JSON.parse(localStorage.getItem('chatMessages') ?? '[]');
 	let scrollToDiv: HTMLDivElement
+
+	function saveConversation() {
+		localStorage.setItem('chatMessages', JSON.stringify(chatMessages));
+	}
 
 	function scrollToBottom() {
 		setTimeout(function () {
@@ -17,7 +21,8 @@
 
 	const handleSubmit = async () => {
 		loading = true
-		chatMessages = [...chatMessages, { role: 'user', content: query }]
+		chatMessages = [...chatMessages, { role: 'user', content: query }];
+		saveConversation();
 
 		const eventSource = new SSE('/api/chat', {
 			headers: {
@@ -36,6 +41,7 @@
 				loading = false
 				if (e.data === '[DONE]') {
 					chatMessages = [...chatMessages, { role: 'assistant', content: answer }]
+					saveConversation();
 					answer = ''
 					return
 				}
@@ -61,47 +67,48 @@
 		console.error(err)
 	}
 </script>
+
 <style>
 	.parent-container {
-      height: 100vh;
-     }
-	 
-	 .chat-container {
-	  height: calc(99vh - 3.5rem); /* Subtract the height of the input area */
-	  padding-bottom: env(safe-area-inset-bottom); /* Add padding for the virtual keyboard on mobile devices */
+		height: 100vh;
+	}
+
+	.chat-container {
+		height: calc(99vh - 3.5rem); /* Subtract the height of the input area */
+		padding-bottom: env(safe-area-inset-bottom); /* Add padding for the virtual keyboard on mobile devices */
 	}
 	.input-area {
-	  position: fixed;
-	  bottom: 0;
-	  left: 0;
-	  right: 0;
-	  padding: 0 env(safe-area-inset-left) 0 env(safe-area-inset-right); /* Add padding for the virtual keyboard on mobile devices */
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		padding: 0 env(safe-area-inset-left) 0 env(safe-area-inset-right); /* Add padding for the virtual keyboard on mobile devices */
 	}
-	
-  </style>
+</style>
+
 <div class="flex flex-col w-full px-0 items-center h-full parent-container">
-  <div class="flex flex-col w-full px-0 items-center h-full">
-	<div class="chat-container w-full bg-gray-900 rounded-md p-4 overflow-y-auto flex flex-col gap-4">
-	  <div class="flex flex-col gap-2">
-		<ChatMessage type="assistant" message="Good morrow! I am William Shakespeare, the Bard of Avon. Let us delve into the poetic beauty of language and the human experience." />
-		{#each chatMessages as message}
-		  <ChatMessage type={message.role} message={message.content} />
-		{/each}
-		{#if answer}
-		  <ChatMessage type="assistant" message={answer} />
-		{/if}
-		{#if loading}
-		  <ChatMessage type="assistant" message="Loading.." />
-		{/if}
-	  </div>
-	  <div class="" bind:this={scrollToDiv} />
+	<div class="flex flex-col w-full px-0 items-center h-full">
+		<div class="chat-container w-full bg-gray-900 rounded-md p-4 overflow-y-auto flex flex-col gap-4">
+			<div class="flex flex-col gap-2">
+				<ChatMessage type="assistant" message="Good morrow! I am William Shakespeare, the Bard of Avon. Let us delve into the poetic beauty of language and the human experience." />
+				{#each chatMessages as message}
+					<ChatMessage type={message.role} message={message.content} />
+				{/each}
+				{#if answer}
+					<ChatMessage type="assistant" message={answer} />
+				{/if}
+				{#if loading}
+					<ChatMessage type="assistant" message="Loading.." />
+				{/if}
+			</div>
+			<div class="" bind:this={scrollToDiv} />
+		</div>
+		<form
+			class="input-area flex w-full rounded-md gap-4 bg-gray-900 p-4"
+			on:submit|preventDefault={() => handleSubmit()}
+		>
+			<input type="text" class="input input-bordered flex-grow" bind:value={query} />
+			<button type="submit" class="btn btn-accent"> Send </button>
+		</form>
 	</div>
-	<form
-	  class="input-area flex w-full rounded-md gap-4 bg-gray-900 p-4"
-	  on:submit|preventDefault={() => handleSubmit()}
-	>
-	  <input type="text" class="input input-bordered flex-grow" bind:value={query} />
-	  <button type="submit" class="btn btn-accent"> Send </button>
-	</form>
-  </div>
 </div>
