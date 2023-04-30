be99c5f10cc04e078801f4d2b97f3bd57bfbfa30
Steven S <sumpter32@gmail.com>
2023-05-01 07:48:13 +0800

    fix chat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,43 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import ChatMessage from '$lib/components/ChatMessage.svelte'
-	import type { ChatCompletionRequestMessage } from 'openai'
-	import { SSE } from 'sse.js'
+  import { onMount } from 'svelte';
+  import ChatMessage from '$lib/components/ChatMessage.svelte'
+  import type { ChatCompletionRequestMessage } from 'openai'
+  import { SSE } from 'sse.js'
+
+  function saveChatMessages() {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('chatMessages', JSON.stringify(chatMessages));
+    }
+  }
+	
+  function loadChatMessages() {
+    if (typeof window !== 'undefined') {
+      const loadedMessages = localStorage.getItem('chatMessages');
+      return loadedMessages ? JSON.parse(loadedMessages) : [];
+    } else {
+      return [];
+    }
+  }
   
-	function saveChatMessages() {
-	  if (typeof window !== 'undefined') {
-		localStorage.setItem('chatMessages', JSON.stringify(chatMessages));
-	  }
-	}
-	  
-	function loadChatMessages() {
-	  if (typeof window !== 'undefined') {
-		const loadedMessages = localStorage.getItem('chatMessages');
-		return loadedMessages ? JSON.parse(loadedMessages) : [];
-	  } else {
-		return [];
-	  }
-	}
-  
-	let query: string = ''
-	let answer: string = ''
-	let loading: boolean = false
-	export let chatMessages: ChatCompletionRequestMessage[] = []
-  
-	let scrollToDiv: HTMLDivElement
-  
-	function scrollToBottom() {
-	  setTimeout(function () {
-		scrollToDiv.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
-	  }, 100)
-	}
-  
-	const handleSubmit = async () => {
+  function clearChat() {
+	chatMessages = [];
+	saveChatMessages();
+}
+
+  let query: string = ''
+  let answer: string = ''
+  let loading: boolean = false
+  export let chatMessages: ChatCompletionRequestMessage[] = []
+
+  let scrollToDiv: HTMLDivElement
+
+  function scrollToBottom() {
+    setTimeout(function () {
+      scrollToDiv.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
+    }, 100)
+  }
+
+  const handleSubmit = async () => {
     loading = true;
     chatMessages = [...chatMessages, { role: 'user', content: query }];
     saveChatMessages();
@@ -80,11 +85,6 @@
     console.error(err)
   }
 
-  const clearChat = () => {
-    chatMessages = [];
-    saveChatMessages();
-  }
-
   onMount(() => {
     chatMessages = loadChatMessages(); // load chat messages on mount
   })
@@ -106,19 +106,42 @@
 	  right: 0;
 	  padding: 0 env(safe-area-inset-left) 0 env(safe-area-inset-right); /* Add padding for the virtual keyboard on mobile devices */
 	}
-	
-        .btn img {
-         width: 24px;
-         height: 24px;
-        }
+	.btn {
+  	  padding-left: 20px; /* set your desired left padding */
+  	  padding-right: 20px; /* set your desired right padding */
+  	}
+
+ 	 .btn img {
+  	    width: 24px;
+   	    height: 24px;
+          }
+  
+ 	 @media (min-width: 768px) {
+  	  .btn img {
+     	   width: 48px;
+           height: 48px;
+    }
+  }
 	
   </style>
 <div class="flex flex-col w-full px-0 items-center h-full parent-container">
   <div class="flex flex-col w-full px-0 items-center h-full">
-    <div class="chat-container w-full bg-gray-900 rounded-md p-4 overflow-y-auto flex flex-col gap-4">
-      <!-- ... same as before -->
-    </div>
-   <form
+	<div class="chat-container w-full bg-gray-900 rounded-md p-4 overflow-y-auto flex flex-col gap-4">
+	  <div class="flex flex-col gap-2">
+		<ChatMessage type="assistant" message="Welcome, good gentle! Thou art about to embark on a grand adventure, penned by none other than the Bard himself, William Shakespeare. In this tale of time and tumult, thou art the hero, a time traveler from the future, tasked with averting a catastrophic event. Pray, engage with the Bard, answer his queries, and shape thy story. Forsooth, thy choices will determine the course of history. Be prepared to face time paradoxes and the consequences of altering the past. Now, let us away to our adventure!" />
+		{#each chatMessages as message}
+		  <ChatMessage type={message.role} message={message.content} />
+		{/each}
+		{#if answer}
+		  <ChatMessage type="assistant" message={answer} />
+		{/if}
+		{#if loading}
+		  <ChatMessage type="assistant" message="Loading.." />
+		{/if}
+	  </div>
+	  <div class="" bind:this={scrollToDiv} />
+	</div>
+	<form
   class="input-area flex w-full rounded-md gap-4 bg-gray-900 p-4"
   on:submit|preventDefault={() => handleSubmit()}
 >
@@ -130,5 +153,6 @@
     <img src="./send.png" alt="Send" />
   </button>
 </form>
+
   </div>
 </div>
