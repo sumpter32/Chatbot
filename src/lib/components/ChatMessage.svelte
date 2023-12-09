ae4e857cdb9b7412e32e3f957a4c1a999f046c49
Steven S <sumpter32@gmail.com>
2023-12-10 06:12:21 +0800

    Update ChatMessage.svelte

--- src/lib/components/ChatMessage.svelte
+++ src/lib/components/ChatMessage.svelte
@@ -2,21 +2,40 @@
 	import type { ChatCompletionRequestMessageRoleEnum } from 'openai'
 	export let type: ChatCompletionRequestMessageRoleEnum
 	export let message: string
-</script>
-
-<div class="chat {type === 'user' ? 'chat-end' : 'chat-start'} justify-end">
+	export let sent: boolean = false // New property to indicate if the message has been sent
+	export let isTyping: boolean = false // New property to indicate if the assistant is typing
+  </script>
+  
+  <div class="chat {type === 'user' ? 'chat-end' : 'chat-start'} justify-end">
 	<div class="chat-image avatar">
-		<div class="w-10 rounded-full">
-			<img
-  src="{type === 'user' ? 'https://ui-avatars.com/api/?name=Me' : './assistant-icon.png'}"
-  alt="{type} avatar"
-/>
-		</div>
+	  <div class="w-10 rounded-full">
+		<img src="{type === 'user' ? 'https://ui-avatars.com/api/?name=Me' : './assistant-icon.png'}" alt="{type} avatar" />
+	  </div>
 	</div>
 	<div class="chat-header">
-		{type === 'user' ? '' : ''}
+	  {type === 'user' ? '' : ''}
 	</div>
 	<div class="chat-bubble {type === 'user' ? 'chat-bubble-primary' : 'chat-bubble-secondary'}">
-		{message}
+	  {message}
+	  {#if sent}
+		<span class="sent-indicator">✓</span>
+	  {/if}
+	  {#if isTyping}
+		<span class="typing-indicator">...</span>
+	  {/if}
 	</div>
-</div>
+  </div>
+  
+  <style>
+	/* Add these styles for the sent and typing indicators */
+	.sent-indicator {
+	  margin-left: 4px;
+	  color: #2ecc71; /* Set the color to a green shade */
+	}
+  
+	.typing-indicator {
+	  font-size: 0.8em;
+	  color: #3498db; /* Set the color to a blue shade */
+	}
+  </style>
+  
